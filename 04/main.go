edfc77adbb4f04fb87afee87b09bf65ccd2bd304
Oleksii Troian <otroian@provectus.com>
2025-12-04 11:19:13 -0500

    day 4 task 2

--- 04/main.go
+++ 04/main.go
@@ -6,16 +6,17 @@ import (
 	"os"
 )
 
-const GRID_SIZE = 10
+const GRID_SIZE = 137
 
 func main() {
-	file, err := os.Open("test.txt")
+	file, err := os.Open("input.txt")
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 
-	var grid [GRID_SIZE + 2][GRID_SIZE + 2]int
+	var grid [GRID_SIZE][GRID_SIZE]bool
+	var neighbours [GRID_SIZE + 2][GRID_SIZE + 2]int
 
 	scanner := bufio.NewScanner(file)
 	i := 1
@@ -24,27 +25,45 @@ func main() {
 		line := scanner.Text()
 		for _, cell := range line {
 			if cell == '@' {
-				grid[i-1][j-1]++
-				grid[i-1][j]++
-				grid[i-1][j+1]++
-				grid[i][j-1]++
-				grid[i][j+1]++
-				grid[i+1][j-1]++
-				grid[i+1][j]++
-				grid[i+1][j+1]++
+				grid[i-1][j-1] = true
+				neighbours[i-1][j-1]++
+				neighbours[i-1][j]++
+				neighbours[i-1][j+1]++
+				neighbours[i][j-1]++
+				neighbours[i][j+1]++
+				neighbours[i+1][j-1]++
+				neighbours[i+1][j]++
+				neighbours[i+1][j+1]++
 			} else {
-				grid[i][j] += 99
+				grid[i-1][j-1] = false
 			}
 			j++
 		}
 		i++
 	}
 	answer := 0
-	for i := 1; i < GRID_SIZE+1; i++ {
+	i = 1
+	for i < GRID_SIZE+1 {
+		rollRemoved := false
 		for j := 1; j < GRID_SIZE+1; j++ {
-			if grid[i][j] < 4 {
+			if grid[i-1][j-1] && neighbours[i][j] < 4 {
 				answer++
+				rollRemoved = true
+				grid[i-1][j-1] = false
+				neighbours[i-1][j-1]--
+				neighbours[i-1][j]--
+				neighbours[i-1][j+1]--
+				neighbours[i][j-1]--
+				neighbours[i][j+1]--
+				neighbours[i+1][j-1]--
+				neighbours[i+1][j]--
+				neighbours[i+1][j+1]--
 			}
+		}
+		if rollRemoved {
+			i = max(1, i-1) // if a roll was removed go back 1 row if possible
+		} else {
+			i++
 		}
 	}
 	fmt.Println(answer)
