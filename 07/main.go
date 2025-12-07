1a0b2358e67349c50f0395bfceadbd1ea55f3966
Oleksii Troian <otroian@provectus.com>
2025-12-07 10:12:39 -0500

    day 7 task 1

--- 07/main.go
+++ 07/main.go
@@ -3,17 +3,39 @@ package main
 import (
 	"bufio"
 	"os"
+	"fmt"
 )
 
 func main() {
-	file, err := os.Open("test.txt")
+	file, err := os.Open("input.txt")
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		scanner.Text()
+	scanner.Scan()
+	line := scanner.Text()
+	beamPos := make(map[int]struct{})
+	for i := range line {
+		if line[i] == 'S' {
+			beamPos[i] = struct{}{}
+			break
+		}
 	}
+	splits := 0
+	for scanner.Scan() {
+		line = scanner.Text()
+		for i, c := range line {
+			if c == '^' {
+				if _, ok := beamPos[i]; ok {
+					splits++
+					delete(beamPos, i)
+					beamPos[i-1] = struct{}{}
+					beamPos[i+1] = struct{}{}
+				}
+			}
+		}
+	}
+	fmt.Println(splits)
 }
