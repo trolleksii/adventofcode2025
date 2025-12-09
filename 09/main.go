81e58144f2733143fa667ea09f03bd5d2c785158
Oleksii Troian <otroian@estee.com>
2025-12-09 09:29:22 -0500

    day 1 task 1

--- 09/main.go
+++ 09/main.go
@@ -3,17 +3,34 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"math"
 	"os"
 	"strconv"
 	"strings"
 )
 
 type Coordinate struct {
-	x, y int
+	x, y float64
+}
+
+type Pair struct {
+	c1, c2 Coordinate
+	area   float64
+}
+
+func NewPair(c1, c2 Coordinate) Pair {
+	a := squareArea(c1, c2)
+	return Pair{c1: c1, c2: c2, area: a}
+}
+
+func squareArea(a, b Coordinate) float64 {
+	w := math.Abs(a.x - b.x) + 1
+	h := math.Abs(a.y - b.y) + 1
+	return w * h
 }
 
 func main() {
-	file, err := os.Open("test.txt")
+	file, err := os.Open("input.txt")
 	if err != nil {
 		panic(err)
 	}
@@ -31,7 +48,21 @@ func main() {
 		if err != nil {
 			panic(err)
 		}
-		redTiles = append(redTiles, Coordinate{x: x, y:y})
+		redTiles = append(redTiles, Coordinate{x: float64(x), y: float64(y)})
 	}
-	fmt.Println(redTiles)
+
+	coordinatePairs := []Pair{}
+	var maxAreaPair Pair
+	l := len(redTiles)
+	for i := range redTiles {
+		for j := i + 1; j < l; j++ {
+			p := NewPair(redTiles[i], redTiles[j])
+			if p.area > maxAreaPair.area {
+				maxAreaPair = p
+			}
+			coordinatePairs = append(coordinatePairs, p)
+		}
+	}
+
+	fmt.Println(int(maxAreaPair.area))
 }
