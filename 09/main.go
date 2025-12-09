98d334c017e3ad6577e96a444ed6137e0cf312ed
Oleksii Troian <otroian@estee.com>
2025-12-09 09:14:17 -0500

    begin d9

--- 09/main.go
+++ 09/main.go
@@ -4,7 +4,13 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
+
+type Coordinate struct {
+	x, y int
+}
 
 func main() {
 	file, err := os.Open("test.txt")
@@ -13,9 +19,19 @@ func main() {
 	}
 	defer file.Close()
 
+	redTiles := []Coordinate{}
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		scanner.Text()
+		line := strings.Split(scanner.Text(), ",")
+		x, err := strconv.Atoi(line[0])
+		if err != nil {
+			panic(err)
+		}
+		y, err := strconv.Atoi(line[1])
+		if err != nil {
+			panic(err)
+		}
+		redTiles = append(redTiles, Coordinate{x: x, y:y})
 	}
-	fmt.Println("result")
+	fmt.Println(redTiles)
 }
