dd539fcf6b922a1c7d47c27a873889e0542a0c97
Oleksii Troian <otroian@estee.com>
2025-12-11 15:04:30 -0500

    d10t1

--- 10/main.go
+++ 10/main.go
@@ -4,18 +4,30 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	file, err := os.Open("test.txt")
-	if err != nil {
-		panic(err)
-	}
+	file, _ := os.Open("test.txt")
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
+	puzzle := make(map[string][][]int)
 	for scanner.Scan() {
-		scanner.Text()
+		line := strings.Split(scanner.Text(), " ")
+		diagram := line[0][1:len(line[0])-1]
+		//joltage := line[len(line)-1]
+		buttons := [][]int{}
+		for _, b := range line[1:len(line)-1] {
+			combo := []int{}
+			for _, i := range strings.Split(b[1:len(b)-1], ",") {
+				num, _  := strconv.Atoi(i)
+				combo = append(combo, num)
+			}
+			buttons = append(buttons, combo)
+		}
+		puzzle[diagram] = buttons
 	}
-	fmt.Println("result")
+	fmt.Println(puzzle)
 }
