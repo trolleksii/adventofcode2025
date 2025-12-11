24ee59fea4271db77b65185af9a411dbe99d925b
Oleksii Troian <otroian@estee.com>
2025-12-11 12:22:18 -0500

    d11 t1

--- 11/main.go
+++ 11/main.go
@@ -4,18 +4,35 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	file, err := os.Open("test.txt")
+	file, err := os.Open("input.txt")
 	if err != nil {
 		panic(err)
 	}
 	defer file.Close()
 
+	graph := make(map[string][]string)
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
-		scanner.Text()
+		line := strings.Split(scanner.Text(), ":")
+		name := line[0]
+		neighbours := strings.Fields(line[1])
+		graph[name] = neighbours
 	}
-	fmt.Println("result")
+	paths := 0
+	queue := []string{"you"}
+	visited := make(map[string]bool)
+	for len(queue) > 0 {
+		current := queue[0]
+		queue = queue[1:]
+		if current == "out" {
+			paths++
+		}
+		visited[current] = true
+		queue = append(queue, graph[current]...)
+	}
+	fmt.Println(paths)
 }
