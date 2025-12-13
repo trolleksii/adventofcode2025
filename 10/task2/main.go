4bb8637b0204f3919d9f12db474dc8eed470fb7c
Oleksii Troian <otroian@estee.com>
2025-12-13 09:42:13 -0500

    wip

--- 10/task2/main.go
+++ 10/task2/main.go
@@ -4,20 +4,24 @@ import (
 	"bufio"
 	"container/heap"
 	"fmt"
-	"math"
 	"os"
 	"slices"
 	"strconv"
 	"strings"
 )
 
-type Button struct {
-	value      int
-	maxPresses int
+type Button []int
+
+func (b Button) connectLight(num int) {
+	b[num] = 1
+}
+
+func NewButton(size int) Button {
+	return make([]int, size)
 }
 
 type Puzzle struct {
-	joltageReq int
+	joltageReq []int
 	seqLen     int
 	buttons    []Button
 }
@@ -60,27 +64,22 @@ func main() {
 		joltageData := data[len(data)-1]
 		joltageData = joltageData[1 : len(joltageData)-1]
 		joltages := strings.Split(joltageData, ",")
-		joltageNum := 0
-		maxLightPresses := []int{}
+		joltageReqs := make([]int, len(joltages))
 		for i, item := range joltages {
-			n, _ := strconv.Atoi(item)
-			maxLightPresses = append(maxLightPresses, n)
-			joltageNum += n * int(math.Pow10(len(joltages)-i-1))
+			joltageReqs[i], _ = strconv.Atoi(item)
 		}
 
 		buttons := []Button{}
-		for _, b := range data[1 : len(data)-1] {
-			button := 0
-			maxPresses := 999
-			for i := range strings.SplitSeq(b[1:len(b)-1], ",") {
+		for _, bs := range data[1 : len(data)-1] {
+			lights := strings.Split(bs[1:len(bs)-1], ",")
+			button := NewButton(len(joltageReqs))
+			for _, i := range lights {
 				lightIndex, _ := strconv.Atoi(i)
-				maxPresses = min(maxPresses, maxLightPresses[lightIndex])
-				button += int(math.Pow10(len(joltages) - lightIndex - 1))
+				button.connectLight(lightIndex)
 			}
-			buttons = append(buttons, Button{value: button, maxPresses: maxPresses})
+			buttons = append(buttons, button)
 		}
-		slices.SortFunc(buttons, func(a, b Button) int { return b.value - a.value })
-		puzzles = append(puzzles, Puzzle{joltageReq: joltageNum, buttons: buttons})
+		puzzles = append(puzzles, Puzzle{joltageReq: joltageReqs, buttons: buttons})
 	}
 	total := 0
 	for _, puzzle := range puzzles {
@@ -91,19 +90,53 @@ func main() {
 	fmt.Println(total)
 }
 
+// subtracts b from a, k times. Returns new slice and a "net balance" which is 0 if resulting slice has all 0, +1 if resulting slice has no negative numbers, -1 if has any negative numbers
+func sub(a, b []int, k int) ([]int, int) {
+	isEmpty := true
+	hasNegatives := false
+	c := make([]int, len(a))
+	for i := range a {
+		c[i] = a[i] - b[i]*k
+		if c[i] != 0 {
+			isEmpty = false
+		}
+		if c[i] < 0 {
+			hasNegatives = true
+		}
+	}
+	netBalance := 1
+	if hasNegatives {
+		netBalance = -1
+	} else if isEmpty {
+		netBalance = 0
+	}
+	return c, netBalance
+}
+
+func getMaxPresses(button, joltage []int) int {
+	maxPresses := 999
+	for i, v := range button {
+		if v != 0 {
+			maxPresses = min(maxPresses, joltage[i] / v)
+		}
+	}
+	return maxPresses
+}
+
 func getShortestSequence(puzzle Puzzle) int {
 	queue := &PriorityQueue{puzzle}
 	heap.Init(queue)
 	for queue.Len() > 0 {
 		subPuzzle := heap.Pop(queue).(Puzzle)
 		for buttonIndex, button := range subPuzzle.buttons {
-			if button.value > subPuzzle.joltageReq {
+			_, balance := sub(subPuzzle.joltageReq, button, 1)
+			if balance < 0 {
 				continue
-			} else if subPuzzle.joltageReq == button.value {
+			} else if balance == 0 {
 				return subPuzzle.seqLen + 1
 			}
-			for i := button.maxPresses; i >= 0; i-- {
-				newJoltageReq := subPuzzle.joltageReq - button.value*i
+			for i := getMaxPresses(button, subPuzzle.joltageReq); i >= 0; i-- {
+				newJoltageReq, _ := sub(subPuzzle.joltageReq, button, i)
 				newSubPuzzle := Puzzle{seqLen: subPuzzle.seqLen + i, joltageReq: newJoltageReq, buttons: slices.Concat(subPuzzle.buttons[:buttonIndex], subPuzzle.buttons[buttonIndex+1:])}
 				heap.Push(queue, newSubPuzzle)
 			}
