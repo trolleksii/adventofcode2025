9d22977c1e8095286d3634e63c07b4b4900b59bd
Oleksii Troian <otroian@estee.com>
2025-12-15 14:22:56 -0500

    day 11 task 2

--- 11/main.go
+++ 11/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"slices"
 	"strings"
 )
 
@@ -22,17 +23,55 @@ func main() {
 		neighbours := strings.Fields(line[1])
 		graph[name] = neighbours
 	}
-	paths := 0
-	queue := []string{"you"}
 	visited := make(map[string]bool)
-	for len(queue) > 0 {
-		current := queue[0]
-		queue = queue[1:]
-		if current == "out" {
-			paths++
-		}
-		visited[current] = true
-		queue = append(queue, graph[current]...)
-	}
-	fmt.Println(paths)
+	required := map[string]bool{"fft": true, "dac": true}
+
+	memo := make(map[State]int)
+	fmt.Println(countPathsWithRequired(graph, "svr", visited, required, memo))
+}
+
+type State struct {
+    node string
+    reqKey string
+}
+
+func countPathsWithRequired(graph map[string][]string, current string, visited map[string]bool, required map[string]bool, memo map[State]int) int {
+ 	reqNodes := make([]string, 0, len(required))
+    for k := range required {
+        reqNodes = append(reqNodes, k)
+    }
+    slices.Sort(reqNodes)
+    state := State{current, strings.Join(reqNodes, ",")}
+    
+    if val, ok := memo[state]; ok {
+        return val
+    }
+    
+    if current == "out" {
+        if len(required) == 0 {
+            return 1
+        }
+        return 0
+    }
+
+    visited[current] = true
+    wasRequired := required[current]
+    if wasRequired {
+        delete(required, current)
+    }
+
+    count := 0
+    for _, neighbor := range graph[current] {
+        if !visited[neighbor] {
+            count += countPathsWithRequired(graph, neighbor, visited, required, memo)
+        }
+    }
+
+    visited[current] = false
+    if wasRequired {
+        required[current] = true
+    }
+    
+    memo[state] = count
+    return count
 }
