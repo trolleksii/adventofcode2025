132dbec65cc0702f02d473a651a31b8bdbf33b51
Oleksii Troian <otroian@estee.com>
2025-12-15 14:21:47 -0500

    day9 task 2

--- 09/main.go
+++ 09/main.go
@@ -9,133 +9,104 @@ import (
 	"strings"
 )
 
-type Coordinate struct {
-	x, y int
+type Vertex struct {
+	x, y float64
 }
 
-func squareArea(a, b Coordinate) int {
-	w := math.Abs(float64(a.x-b.x)) + 1
-	h := math.Abs(float64(a.y-b.y)) + 1
+type Edge struct {
+	fixed, from, to float64
+}
+
+func squareArea(a, b Vertex) int {
+	w := math.Abs(a.x-b.x) + 1
+	h := math.Abs(a.y-b.y) + 1
 	return int(w * h)
 }
 
-func getCoordinates(line string) Coordinate {
+func getCoordinates(line string) Vertex {
 	p := strings.Split(line, ",")
-	x, err := strconv.Atoi(p[0])
-	if err != nil {
-		panic(err)
-	}
-	y, err := strconv.Atoi(p[1])
-	if err != nil {
-		panic(err)
-	}
-	return Coordinate{x: x, y: y}
+	x, _ := strconv.Atoi(p[0])
+	y, _ := strconv.Atoi(p[1])
+	return Vertex{x: float64(x), y: float64(y)}
 }
 
-func main() {
-	file, err := os.Open("test.txt")
-	if err != nil {
-		panic(err)
-	}
+func Parse(filename string) ([]Vertex, []Edge, []Edge) {
+	file, _ := os.Open(filename)
 	defer file.Close()
 
-	redTiles := []Coordinate{}
-	reds := make(map[Coordinate]bool)
-	greens := make(map[Coordinate]bool)
+	redTiles := []Vertex{}
+	vedges := []Edge{}
+	hedges := []Edge{}
 	scanner := bufio.NewScanner(file)
 	scanner.Scan()
 	this := getCoordinates(scanner.Text())
 	redTiles = append(redTiles, this)
-	reds[this] = true
-	minX, maxX := this.x, this.x
-	minY, maxY := this.y, this.y
 	for scanner.Scan() {
 		next := getCoordinates(scanner.Text())
 		redTiles = append(redTiles, next)
-		if next.x < minX {
-			minX = next.x
-		}
-		if next.x > maxX {
-			maxX = next.x
-		}
-		if next.y < minY {
-			minY = next.y
-		}
-		if next.y > maxY {
-			maxY = next.y
-		}
-		reds[next] = true
-		// paint the vertical borders
 		if next.x == this.x {
-			if next.y > this.y {
-				for i := this.y + 1; i < next.y; i++ {
-					greens[Coordinate{x: this.x, y: i}] = true
-				}
-			} else {
-				for i := next.y + 1; i < this.y; i++ {
-					greens[Coordinate{x: this.x, y: i}] = true
-				}
-			}
+			vedges = append(vedges, Edge{fixed: this.x, from: min(this.y, next.y), to: max(this.y, next.y)})
+		} else {
+			hedges = append(hedges, Edge{fixed: this.y, from: min(this.x, next.x), to: max(this.x, next.x)})
 		}
 		this = next
 	}
-	// connect last point with the first point
-	first := redTiles[0]
-	if first.x == this.x {
-		if first.y > this.y {
-			for i := this.y + 1; i < first.y; i++ {
-				greens[Coordinate{x: this.x, y: i}] = true
-			}
-		} else {
-			for i := first.y + 1; i < this.y; i++ {
-				greens[Coordinate{x: this.x, y: i}] = true
-			}
-		}
+	next := redTiles[0]
+	if this.x == next.x {
+		vedges = append(vedges, Edge{fixed: this.x, from: min(this.y, next.y), to: max(this.y, next.y)})
+	} else {
+		hedges = append(hedges, Edge{fixed: this.y, from: min(this.x, next.x), to: max(this.x, next.x)})
 	}
-	for y := minY; y <= maxY; y++ {
-		inside := false
-		for x := minX; x <= maxX; x++ {
-			c := Coordinate{x: x, y: y}
+	return redTiles, vedges, hedges
+}
 
-			if greens[c] {
-				// It's an edge - but is it a vertical edge?
-				// Check if it connects tiles above AND below
-				above := Coordinate{x: x, y: y - 1}
-				below := Coordinate{x: x, y: y + 1}
-				if (greens[above] || reds[above]) && (greens[below] || reds[below]) {
-					inside = !inside
-				}
-				continue
-			}
-
-			if reds[c] {
-				// Same check for red tiles
-				above := Coordinate{x: x, y: y - 1}
-				below := Coordinate{x: x, y: y + 1}
-				if (greens[above] || reds[above]) && (greens[below] || reds[below]) {
-					inside = !inside
-				}
-				continue
-			}
-
-			if inside {
-				greens[c] = true
-			}
+func noEdgesInside(minx, miny, maxx, maxy float64, vedges []Edge, hedges []Edge) bool {
+	for _, e := range vedges {
+		if e.fixed > minx && e.fixed < maxx && e.to > miny && e.from < maxy {
+			return false
 		}
 	}
 
-	for y := range 9 {
-		row := make([]byte, 14)
-		for x := range 14 {
-			c := Coordinate{x: x, y: y}
-			if reds[c] {
-				row[x] = '#'
-			} else if greens[c] {
-				row[x] = 'X'
+	for _, e := range hedges {
+		if e.fixed > miny && e.fixed < maxy && e.to > minx && e.from < maxx {
+			return false
+		}
+	}
+	return true
+}
+
+func isInside(x, y float64, edges []Edge) bool {
+	intersections := 0
+	for _, e := range edges {
+		if e.fixed > x {
+			if e.from <= y && y < e.to {
+				intersections++
+			}
+		}
+	}
+	return intersections%2 == 1
+}
+
+func main() {
+	redTiles, vedges, hedges := Parse("input.txt")
+	maxArea := 0
+	for i, v1 := range redTiles {
+		for j := i + 1; j < len(redTiles); j++ {
+			v2 := redTiles[j]
+			if area := squareArea(v1, v2); area < maxArea {
+				continue
 			} else {
-				row[x] = '.'
+				minx := min(v1.x, v2.x)
+				miny := min(v1.y, v2.y)
+				maxx := max(v1.x, v2.x)
+				maxy := max(v1.y, v2.y)
+				if isInside(minx+0.5, miny+0.5, vedges) {
+					if noEdgesInside(minx, miny, maxx, maxy, vedges, hedges) {
+						maxArea = area
+					}
+				}
 			}
 		}
-		fmt.Println(string(row))
 	}
+	fmt.Println(maxArea)
 }
